509ffb82fdebba50979e6ebb5d6f38b2cccb2562
fatpav <fat_pav@hotmail.com>
2020-10-20 16:14:51 +0100

    list rendered, time to style

--- src/components/ShoppingListItem.vue
+++ src/components/ShoppingListItem.vue
@@ -1,15 +1,28 @@
 <template lang="html">
-  <p>{{ item.name }}</p>
+  <p :class='purchasedClass'>{{ item.name }}</p>
 </template>
 
 <script>
 export default {
     name: 'shopping-list-item',
-    props: ['item']
+    props: ['item'],
+    computed: {
+        purchasedClass: function () {
+            return this.item.isPurchased ? 'purchased' : 'not-purchased'
+        }
+    }
 
 }
 </script>
 
 <style lang="css" scoped>
+.not-purchased {
+    background-color: gold;
+}
+
+.purchased {
+    text-decoration: line-through;
+    background-color: green;
+}
 
 </style>
